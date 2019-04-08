c012610db1d6fac183fd611b53b305f7dedad159
256hz <a.d@256hz.com>
2019-04-08 13:43:19 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  # , only: [:index, :new, :show]
+  # # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
+  # post 'articles', to: 'articles#show'
+  #
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: 'Title1', description: 'its a article')
+Article.create(title: 'Title2', description: 'its kinda a article')
+Article.create(title: 'Title3', description: 'its maybe a article')
